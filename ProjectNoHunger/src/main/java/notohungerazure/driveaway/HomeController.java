afa3798d802ede3d864a1acd8bf558c17c0c8e43
SubhaArun-N <55850824+SubhaArun-N@users.noreply.github.com>
2020-03-12 19:28:34 -0400

    Revert "change origin *"
    This reverts commit 02be55ed41b64f7f7eebb125126882b63a648821.

--- ProjectNoHunger/src/main/java/notohungerazure/driveaway/HomeController.java
+++ ProjectNoHunger/src/main/java/notohungerazure/driveaway/HomeController.java
@@ -24,7 +24,7 @@ import notohungerazure.driveaway.models.Request;
 
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins="http://nohunger.azurewebsites.net")
 @RequestMapping(path = "no-hunger")
 public class HomeController {
 	
